1fa631e916307856bdef5e5ff6512b3044829f94
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-08-30 19:56:43 +0300

    A new hotfix of install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 echo '----------------------------------------  Pieno installer 1.0  ----------------------------------------'
 
-pip install -r requirements.txt # Install dependencies
-
 python3 -m venv venv            # Create the virtual enviroment
 source venv/bin/activate        # Activate it
+
+pip install -r requirements.txt # Install dependencies
 
 echo '----------------------------------------    Pieno installed    ----------------------------------------'
 echo
 echo 'Starting Pieno...'
-venv/bin/python3 app.py
+venv/bin/python3
