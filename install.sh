3762265be96b877c5a64e1e41f4c4d3d565268e2
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-09-01 15:39:54 +0300

    Hotfix on install.sh

--- install.sh
+++ install.sh
@@ -1,47 +1,31 @@
 #!/bin/bash
 
-venv_path="venv"
+    echo '---------------------------------------|  Pieno installer 1.3  |---------------------------------------'
 
-echo '---------------------------------------|  Pieno installer 1.2  |---------------------------------------'
-
-if [ ! -d "$(venv_path)" ]; then
+if [ ! -d venv ]; then
     echo "No Python virtual environment in this directory.."
     echo 'Creating Python virtual environment and activating it...'
-    python3 -m venv "$(venv_path)"            # Create the virtual enviroment
-    source "$(venv_path)"/bin/activate        # Activate it
+    python3 -m venv venv            # Create the virtual enviroment
+    source venv/bin/activate        # Activate it
 
     echo 'Installing dependencies...'
     pip install -r requirements.txt # Install dependencies
+    echo "Tasks done: Created Python virtual environment, Installed dependencies"
     echo '-----------------------------------------|  Pieno installed  |-----------------------------------------'
 else
     echo "Virtual environment found."
-    if [ "$("$(venv_path)"/bin/pip3 freeze)" != "$(cat requirements.txt)" ]; then
+    if [ "$(venv/bin/pip3 freeze)" != "$(cat requirements.txt)" ]; then
         echo "Virtual environment doesn't contain right modules."
-        if [ "$(read -rsp "Install needed modules? (Y/n)")" == "y" ] || [ "$(read -rsp "Install needed modules? (Y/n)")" == "Y" ] ; then
-            echo 'Installing dependencies...'
-            pip install -r requirements.txt # Install dependencies
-            echo '-----------------------------------------|  Pieno installed  |-----------------------------------------'
-        elif [ "$(read -rsp "Install needed modules? (Y/n)")" == "n" ] || [ "$(read -rsp "Install needed modules? (Y/n)")" == "N" ]; then
-            echo "To use a different virtual environment, specify the path on the top of the \"install.sh\" file"
-            echo "Exiting..."
-            echo '--------------------------------|  WARNING Pieno not fully installed  |--------------------------------'
-            exit 1
-        fi
-    else
-        echo "All needed modules already installed."
-        echo '-------------------------------------|  Pieno was already installed  |-------------------------------------'
-    fi
-fi
-
-if [ "$(read -rsp "Start Pieno? (Y/n)")" == "y" ] || [ "$(read -rsp "Start Pieno? (Y/n)")" == "Y" ] ; then
         echo 'Installing dependencies...'
         pip install -r requirements.txt # Install dependencies
+        echo "Tasks done: Installed dependencies"
         echo '-----------------------------------------|  Pieno installed  |-----------------------------------------'
-    elif [ "$(read -rsp "Start Pieno? (Y/n)")" == "n" ] || [ "$(read -rsp "Start Pieno? (Y/n)")" == "N" ]; then
-        echo "To use a different virtual environment, specify the path on the top of the \"install.sh\" file"
-        echo "Exiting..."
-        echo '--------------------------------|  WARNING Pieno not fully installed  |--------------------------------'
-        exit 1
+    else
+        echo "All needed modules already installed."
+        echo "Tasks done: None"
+        echo '-----------------------------------|  Pieno was already installed  |-----------------------------------'
     fi
+fi
+echo "Staring pieno..."
 
-exit 0
+venv/bin/python3 ./app.py
